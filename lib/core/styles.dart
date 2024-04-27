f86346be8731628c7229e44cb3e895d5dacf40b8
Farah Seyam <farahseyam1911@gmail.com>
2024-04-27 20:35:50 +0300

    chatting with passenger

---
+++ lib/core/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mute_motion/core/utils/constant.dart';
+
+
+abstract class Styles {
+  static var textStyle15 = TextStyle(
+    fontFamily: 'Comfortaa',
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    color: borderColor,
+  );
+  static var textStyle12 = TextStyle(
+     fontFamily: 'Comfortaa',
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: borderColor,
+  );
+}
